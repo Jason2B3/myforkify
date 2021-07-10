ba9d3c4e136776e8b8365f0199d9729a1185df30
jason <2B3>
2021-07-10 17:36:09 -0400

    fixed ui bug for bookmark previews

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -13,13 +13,11 @@ class bookmarksView extends View {
   _data; // equal to the recipe object
   _generateMarkup() {
     // Create several previews- 1 for each recipe we loaded/placed in _data
-    //! NOT ACTIVATED
     let generateAll = this._bookmarks
       .map(dataObj => {
         return this._generateMarkupPreview(dataObj);
       })
       .join('');
-    console.log(generateAll);
     return generateAll;
   }
 
@@ -32,16 +30,24 @@ class bookmarksView extends View {
   }
   _generateMarkupPreview(result) {
     // Keep a recipe highlighted after clicking on it in the search results list
+    const id = window.location.hash.slice(1);
     return `<li class="preview">
-    <a class="preview__link" href="#${result.id}">
+    <a class="preview__link ${
+      result.id === id ? 'preview__link--active' : ''
+    }" href="#${result.id}">
       <figure class="preview__fig">
         <img src="${result.image}" alt="Test" />
       </figure>
       <div class="preview__data">
-        <h4 class="preview__name">
-          ${result.title}
-        </h4>
+        <h4 class="preview__title">${result.title}</h4>
         <p class="preview__publisher">${result.publisher}</p>
+        <!--
+        <div class="preview__user-generated">
+          <svg>
+            <use href="${icons}#icon-user"></use>
+          </svg>
+        </div>
+        -->
       </div>
     </a>
   </li>`;
